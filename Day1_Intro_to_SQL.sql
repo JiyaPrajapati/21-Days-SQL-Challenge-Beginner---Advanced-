eb80564bd96419320b4b4c74afb5eaba2a981259
JiyaPrajapati <jiyaprajapati080806@gmail.com>
2025-11-03 23:04:45 +0530

    Create Day1_Intro_to_SQL.sql
    # 21-Days SQL Challenge — Day 1
    
    Topic: Introduction to SQL & SELECT statement
    
    Contents:
    - Example tables: patients, services_weekly
    - Practice queries demonstrating SELECT *, column selection, LIMIT, and DISTINCT
    - Commands used: SELECT, SELECT DISTINCT, CREATE TABLE, INSERT, LIMIT

---
+++ Day1_Intro_to_SQL.sql
@@ -0,0 +1,12 @@
+Challenge Question
+List all unique hospital services available in the hospital.
+
+Query :- SELECT DISTINCT service FROM services_weekly;
+
+Output :-
+service
+------------------
+ general_medicine
+ surgery
+ ICU
+ emergency
